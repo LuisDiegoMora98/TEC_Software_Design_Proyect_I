797c010af51c8beb5c44412cb5e63d4d70f14bce
MrLuis137 <lalemn2@gmail.com>
2021-09-29 16:44:55 -0600

    Weapon controller

--- Project1_Character_Library/src/Controller/WeaponController.java
+++ Project1_Character_Library/src/Controller/WeaponController.java
@@ -14,11 +14,23 @@ import Model.IPrototype;
 import Model.WeaponPrototypeFactory;
 import java.util.ArrayList;
 import Model.Weapon;
+import java.awt.Image;
+import java.io.IOException;
+import java.util.HashMap;
+import utils.JSONLoader;
 
 public class WeaponController {
     
+    private final JSONLoader json;
+    
+    public WeaponController() throws IOException{
+    
+        this.json = JSONLoader.getInstance();
+    }
+    
     public Weapon getWeapon(String name){
-        return (Weapon)WeaponPrototypeFactory.getPrototype(name);
+        return (Weapon)WeaponPrototypeFactory.getPrototypeDeepClone(name,1)
+                .get(0);
     }
     
     public ArrayList<Weapon> getManyWeapons(String name, int quantity){
@@ -30,11 +42,28 @@ public class WeaponController {
     }
     
     public void loadWapons(){
-        
+       json.r
     }
     
-    public void saveWeapon(Weapon weapon, String name){
+    public Weapon crateWeapon(int scope, double damage, double explotionRange, 
+        boolean levelIncrease,String name, HashMap<Integer, Image> aspect, 
+        int level, double cost) throws IOException{
+
+        //No se le está pasando el aspect, pero en realidad puede ser una lista de images
+        //y otra de cant de niveles, o un array de array si es posible
+        Weapon weapon = new Weapon.WeaponBuilder().setName(name)
+                .setScope(scope)
+                //.addAspect(aspect)
+                .setDamage(damage)
+                .setExplotionRange(explotionRange)
+                .setLevel(level)
+                .setLevelIncrease(levelIncrease)
+                .setCost(cost)
+                .build();
+        
         WeaponPrototypeFactory.addPrototype(name, weapon);
+        json.writeJSON(weapon);
+        return weapon;
     }
     
 }
