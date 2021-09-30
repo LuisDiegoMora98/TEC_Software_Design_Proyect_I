e1701eeabc7f089133dbefd26d770237b32832e2
LuisDiegoMora98 <aguilarluisdi@hotmail.com>
2021-09-29 18:29:40 -0600

    Update ICreator

--- Project1_Character_Library/src/Model/ICreator.java
+++ Project1_Character_Library/src/Model/ICreator.java
@@ -13,29 +13,18 @@ import java.util.HashMap;
  * @author Natalia
  */
 public abstract class ICreator {
-    
     protected HashMap<String,IPrototype> prototypes = new HashMap<>();       
-    protected ArrayList<IPrototype> clones = new ArrayList<>();
     
     public ICreator(){
-        this.clones = new ArrayList<>();
         this.prototypes = new HashMap<>();
     }
     
-    public ArrayList<IPrototype> getPrototypeDeepClone(String prototypeName, int quantity){    
-        this.clones = new ArrayList<>();
-        for (int i=0; i< quantity; i++){
-             clones.add(prototypes.get(prototypeName).deepClone());
-        }
-        return clones;         
+    public IPrototype getPrototypeDeepClone(String prototypeName, int quantity){    
+            return prototypes.get(prototypeName).deepClone();
     }  
     
-    public ArrayList<IPrototype> getPrototypeClone(String prototypeName, int quantity){
-        this.clones = new ArrayList<>();
-        for (int i=0; i< quantity; i++){
-             clones.add(prototypes.get(prototypeName).clone());
-        }
-        return clones;         
+    public IPrototype getPrototypeClone(String prototypeName, int quantity){
+        return prototypes.get(prototypeName).clone();
     }  
     
     public void addPrototype(String prototypeName,IPrototype prototype){   
